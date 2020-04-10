537f94fb45acbb3fd75ba759c95efdcc30bf5800
ohowao <owinog98@gmail.com>
2020-04-10 13:37:36 +0100

    Adding first commit to directory

---
+++ src/main/java/com/asperger/funwithdi/FunWithDiApplication.java
@@ -0,0 +1,13 @@
+package com.asperger.funwithdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunWithDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asperger/funwithdi/FunWithDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asperger.funwithdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunWithDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
